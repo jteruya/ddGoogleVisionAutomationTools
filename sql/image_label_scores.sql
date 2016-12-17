58e588076efcc87f356731fa48ba6d573691d8c1
Jonathan Teruya <jonathan@doubledutch.me>
2016-12-16 16:04:58 -0800

    new file:   sql/compute_vision_hackday.sql 	new file:   sql/image_label_scores.sql 	new file:   sql/image_text_scores.sql

---
+++ sql/image_label_scores.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS JT.Image_Label_Scores;
+CREATE TABLE JT.Image_Label_Scores (
+        ApplicationId TEXT,
+        Created TIMESTAMP,
+        ImageFileName TEXT,
+        Score_One NUMERIC,
+        Desc_One TEXT,
+        Score_Two NUMERIC,
+        Desc_Two TEXT,
+        Score_Three NUMERIC,
+        Desc_Three TEXT,
+        Score_Four NUMERIC,
+        Desc_Four TEXT,
+        Score_Five NUMERIC,
+        Desc_Five TEXT,
+        Score_Six NUMERIC,
+        Desc_Six TEXT,
+        Score_Seven NUMERIC,
+        Desc_Seven TEXT,
+        Score_Eight NUMERIC,
+        Desc_Eight TEXT,
+        Score_Nine NUMERIC,
+        Desc_Nine TEXT,
+        Score_Ten NUMERIC,
+        Desc_Ten TEXT
+);   
---
+++ sql/image_text_scores.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS JT.Image_Text_Scores;
+CREATE TABLE JT.Image_Text_Scores (
+        ApplicationId TEXT,
+        Created TIMESTAMP,
+        ImageFileName TEXT,
+        Desc_One TEXT,
+        Desc_Two TEXT,
+        Desc_Three TEXT,
+        Desc_Four TEXT,
+        Desc_Five TEXT,
+        Desc_Six TEXT,
+        Desc_Seven TEXT,
+        Desc_Eight TEXT,
+        Desc_Nine TEXT,
+        Desc_Ten TEXT
+); 
